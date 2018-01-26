8fc47c8f6a39e46eb96331fcdc649f682b484234
Test <test@Tests-Air.Dlink>
2018-01-26 10:33:49 +0300

    test_future

---
+++ script.js
@@ -0,0 +1,9 @@
+(function(){
+  var li = document.getElementById('wood1');
+  var li = document.getElementById('wood2');
+  var li = document.getElementById('wood3');
+  var li = document.getElementById('wood4');
+  var li = document.getElementById('wood5');
+  var li = document.getElementById('wood6');
+  var li = document.getElementById('wood7');
+})();
